dc816f05c8975fdc537e7457bea5971636b81ae0
Disendra <91449346+Disendra@users.noreply.github.com>
2024-02-20 21:27:47 +0530

    Update signUp.js

--- git-nodejs/signUp.js
+++ git-nodejs/signUp.js
@@ -1,6 +1,7 @@
 // routes.js
 const express = require('express')
 const db = require('./dbConnection')
+const XLSX = require('xlsx');
 
 const router = express.Router()
 
@@ -74,17 +75,33 @@ router.post('/login', (req, res) => {
 
 function handleQuery(sql, res) {
   db.query(sql, (err, result) => {
-    if (err) {
-      return res.status(500).send({ status: false, message: err.message });
-    } else {
-      return res.send({
-        status: true,
-        records: result,
-        message: 'Details Fetched Successfully'
-      });
-    }
+      if (err) {
+          return res.status(500).send({ status: false, message: err.message });
+      } else {
+          // Convert data to Excel format
+          const worksheet = XLSX.utils.json_to_sheet(result);
+          const workbook = XLSX.utils.book_new();
+          XLSX.utils.book_append_sheet(workbook, worksheet, 'Data');
+
+          // Send Excel file as response
+          res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+          res.setHeader('Content-Disposition', 'attachment; filename=data.xlsx');
+          res.end(XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' }));
+      }
   });
 }
+
+router.get('/downloadLogintable', (req, res) => {
+  const sql = 'SELECT * FROM login_Data';
+  handleQuery(sql, res);
+});
+
+router.get('/downloadLoginInfo', (req, res) => {
+  const sql = 'SELECT * FROM login_Logs';
+  handleQuery(sql, res);
+});
+
+
 
 router.get('/getLoginData', (req, res) => {
   const sql = 'SELECT * FROM login_Data';
@@ -95,6 +112,21 @@ router.get('/loginInfo', (req, res) => {
   const sql = 'SELECT * FROM login_Logs';
   handleQuery(sql, res);
 });
+
+
+
+router.get('/downloadLogintable', (req, res) => {
+  // Convert userData to Excel format
+  const worksheet = XLSX.utils.json_to_sheet(userData);
+  const workbook = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(workbook, worksheet, 'UserData');
+
+  // Send Excel file as response
+  res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+  res.setHeader('Content-Disposition', 'attachment; filename=userData.xlsx');
+  res.end(XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' }));
+});
+
 
 
 
